8931e7d917221cc811d178e5b559092952b04eef
ehmatthes <robertstipp17@gmail.com>
2022-02-09 07:02:09 -0800

    Removed pics

--- src/contstants/quality.js
+++ src/contstants/quality.js
@@ -1,26 +1,26 @@
-import rohs from "../images/rohs-logo.png"
-import iso from "../images/iso-logo.jpeg"
-import minerals from "../images/AS9100D-logo.png"
+// import rohs from "../images/rohs-logo.png"
+// import iso from "../images/iso-logo.jpeg"
+// import minerals from "../images/AS9100D-logo.png"
 export default [
   {
     id: 1,
     label: "AS9100D and ISO 9001:2015",
     content:
       "ISO standards are maintained by the International Organization for Standardization. AS9100 is a widely adopted and standardized quality management system for the aerospace industry that was released in October 1999, by the Society of Automotive Engineers and the European Association of Aerospace Industries. Blake Wire and Cable is AS9100 Rev. D and ISO 9001:2015 Certified. You can depend on the quality of our products and services. In order to achieve certification, we follow the strict guidelines of the business procedures outlined by the standard",
-    pic: { iso },
+    // pic: { iso },
   },
   {
     id: 2,
     label: "RoHS",
     content:
       "RoHs refers to the Restriction of Hazardous Substances. The European Union instituted  RoHS Directive 2002/95/EC which banned six substances (Lead, Cadmium, Mercury, Hexacalent, Chromium, Polybrominated Biphenysis and Polybrominated Diphenyl Ethers) from being used in the manufacturing process. The goal of this directive is to protect the environment and reduce exposure related health risks",
-    image: { rohs },
+    // image: { rohs },
   },
   {
     id: 3,
     label: "Conflict Minerals",
     content:
       "Section 1502 of the Dodd-Frank Wall Street Reform 1999 and Consumer Protection Act defines and regulates conflict minerals.Conflict Minerals - tantalum, tungsten, tin and gold originate from the Democratic Republic of the Congo. Their revenue directly to indirectly benefits armed groups responsible for war and human rights violations in the DRC and adjoining countries.",
-    image: { minerals },
+    // image: { minerals },
   },
 ]
